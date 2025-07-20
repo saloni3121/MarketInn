fcadc7ce56cd2a59518c058a187999378bdff079
saloni3121 <saloni.patel01@gmail.com>
2025-07-20 13:53:53 -0700

    bug resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import SignupPage from './components/auth/SignupPage';
-import MultiStepSignup from './components/auth/MultiStepSignup';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<MultiStepSignup />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
