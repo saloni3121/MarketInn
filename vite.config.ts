cbc0b15b72da4f85c1c0802c9749e7c4846aaa3b
saloni3121 <saloni.patel01@gmail.com>
2025-07-20 13:58:32 -0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', 
+  base: '/MarketInn/', // 👈 This is required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
